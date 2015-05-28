2d6773278f23b26c18604909e211ec06712adf1c
kaesonho <kaesonho@yahoo-inc.com>
2015-05-28 14:03:55 -0700

    fix test

--- tests/unit/index.client.js
+++ tests/unit/index.client.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 'use strict';
 
 describe('ga plugin client', function () {
-    it('dummy', function (done) {
-        expect(true).to.be(true);
+    it('dummy', function () {
+        expect(true).to.eql(true);
     });
 }); 
